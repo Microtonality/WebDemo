c06e427bfe011d552c6593461f7896d97b56eb7f
Microtonality <Rick@Microtonality.net>
2021-11-20 12:42:02 -0500

    fifth commit

--- js/soundFiles.js
+++ js/soundFiles.js
@@ -132,40 +132,6 @@ function generateNoteList(scale,amplitude,soundType)
 //////////////////////////////////////////////////
 //
 // Source: soundFiles.js
-// Function: adjustAmplitude
-//
-// Parameters:
-//   i
-//   numSamples
-//   amplitude
-//
-// Description: This function adjusts the amplitude at the end
-//		of the sound to decrease a click.
-//
-function adjustAmplitude( i, numSamples, amplitude )
-{
-	
-	// See if we need to taper the amplitude value. We are only
-	//		tapering the last 1000 bytes. This prevents a click sound at the end.
-	if( i >= numSamples - 1000 )
-	{
-		// Decrease the amplitude value by 1/1000
-		amplitude -= 0.001;
-		
-		// Don't let it get below zero.
-		if( amplitude < 0 )
-		{
-			amplitude = 0;
-		}
-	}
-	
-	// Return the amplitude value to caller to "remember" where we are.
-	return amplitude;
-}
-
-//////////////////////////////////////////////////
-//
-// Source: soundFiles.js
 // Function: playNote
 //
 // Parameters:
@@ -551,6 +517,46 @@ function getAmplitudes( instrumentIndex, frequency )
 	
 	return retList;
 }
+
+//////////////////////////////////////////////////////
+//
+// Timbre data from https://hackage.haskell.org/package/sharc-timbre
+//
+// It is covered by the following license notice.
+//
+
+//////////////////////////////////////////////////////
+//
+// Copyright Anton Kholomiov 2010
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+// 
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+// 
+//     * Neither the name of Anton Kholomiov nor the names of other
+//       contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Flute note amplitudes.
 var flute_note0 = [261.626,0.09836,0.2957,0.141921,0.079014,0.112871,0.042798,0.029077];
